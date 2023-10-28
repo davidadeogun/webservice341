c3cc7549c580a47179de4855f94036d9d3a7ff08
David Adeogun <adeogundavidt@gmail.com>
2023-10-28 06:06:16 -0400

    updated

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 //home page route of the application
 app.get('/', (req, res) => {
     res.send('David!');
+});
 
-    
 const port = 3000;
 //start the web server
 app.listen(process.env.port || port, () => {
