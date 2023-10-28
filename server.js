0716183459128621536b2cf2bab06e67f1186f72
David Adeogun <adeogundavidt@gmail.com>
2023-10-27 20:04:06 -0400

    lesson1

---
+++ server.js
@@ -0,0 +1,16 @@
+//express web server
+const express = require('express');
+const app = express();
+
+
+//home page route of the application
+app.get('/', (req, res) => {
+    res.send('David!');
+});
+
+
+const port = 3000;
+//start the web server
+app.listen(process.env.port || port, () => {
+    console.log('Web server listening on port ' + (process.env.port || port));
+});
