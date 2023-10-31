7e95d7a3be470f9d8c915efe7ea424e5eabb9b88
David Adeogun <adeogundavidt@gmail.com>
2023-10-31 17:44:40 -0400

    latest

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,32 +1,8 @@
-
-/*const ObjectId = require('mongodb').ObjectId;
-
-const getAll =async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then(users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
-};
-
-const getSingle =async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then(users) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
-    });
-}; */
-
-
-
-
 const fs = require('fs');
 const path = require('path');
 
 const professionalData = (req, res) => {
-    // If temp.json is inside a 'json' folder at the root of your project
-    const dataPath = path.join(__dirname, '..', 'json', 'temp.json');
+    const dataPath = path.join(__dirname, '..', 'json', 'professional.json');
     const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
     res.json(data);
 };
@@ -37,8 +13,6 @@ const usersContacts = (req, res) => {
     const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
     res.json(data);
 };
-
-
 
 
 
