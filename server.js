9288be9c910fc35462666323c9840546b4a4aad8
David Adeogun <adeogundavidt@gmail.com>
2023-11-03 15:11:02 -0400

    ....

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const mongodb = require('./data/database');
 const {professionalData} = require('./controllers/users');
 const app = express();
 
+const cors = require('cors')
 
 
 const port = process.env.PORT || 8080; // default port for local dev
 
+app.use(cors());
+
 app.use('/', require('./routes'));
+
 
 app.use(express.static('frontend'));
 app.use(express.json());
