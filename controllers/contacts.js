d11143a6f0da60f12b30a9a8d733689fbbe1efed
David Adeogun <adeogundavidt@gmail.com>
2023-11-14 19:01:04 -0500

    pushed my original repo to another branch. confuse

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,7 +5,7 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     const result = await mongodb.getDb().collection('contacts').find();
-    result.toArray().then((users) => {
+    result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
     });
@@ -15,7 +15,7 @@ const getAll = async (req, res) => {
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDb().collection('contacts').find({ _id: userId });
-    result.toArray().then((users) => {
+    result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
